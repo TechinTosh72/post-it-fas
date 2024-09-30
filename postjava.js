53afe5754b42a12fcecf5fb1c1eed5b364e38234
Biridun <lmtomioka@gmail.com>
2024-09-30 12:28:17 -0300

    Add files via upload
    pra eu ver dps

---
+++ postjava.js
@@ -0,0 +1,6 @@
+function criar(){
+    var input = document.getElementById('txtnome');
+    var replique = document.getElementById('repliquenome');
+    alert('oi'+ input.value);
+    replique.innerHTML = input.value;
+}
