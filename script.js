ba354138b61a8434e8817271877aa625f9ece7c8
Eduardo Diniz <ed.dinizsil@gmail.com>
2024-10-07 09:58:00 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 function lerNota() {
     var entrada = document.getElementById("inputNota").value;
+    return entrada
+}
+
+function criarNota() {
+    const notaContainer = document.getElementsByClassName(notaContainer);
+
+    const titulo = document.createElement("h1");
+    const texto = document.createElement("p");
     
+    notaContainer.appendChild(titulo);
+    notaContainer.appendChild(texto);
 }
